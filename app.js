a603ac4d4dbb7fe2dfe7d3afa7ff37c5831f548d
bellicose <bellicose.agnostic@gmail.com>
2015-02-03 17:30:26 -0800

    spare_pc

--- app.js
+++ app.js
@@ -2,25 +2,110 @@
  * Created by admin on 2/2/2015.
  */
 
-angular.module('myApp',[]);
+angular.module('myApp', []);
 
 angular.module('myApp').controller('myController', function () {
     var mc = this;
-    mc.user = {
+    mc.user1 = {
         name: 'Bellicose Agnostic',
         address: {
             street: '123 Street',
             city: 'my city',
             zip: 56223
-        }
-    }
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+    mc.user2 = {
+        name: 'Random Guy',
+        address: {
+            street: '123 Street',
+            city: 'my city',
+            zip: 91605
+        },
+        friends: [
+            'rand 1',
+            'rand 2',
+            'rand 3'
+        ]
+    };
+
 });
 
 angular
-.module('myApp')
-.directive('userInfoCard', function () {
+    .module('myApp')
+    .directive('userInfoCard', function () {
         return {
-            template: '<div ng-show="!!mc.user.name">Name: {{mc.user.name}}</div>Address <br> Street: {{mc.user.address.street}} ',
-            restrict: 'E'
+            templateUrl: 'userInfoCardTemplate.html',
+            restrict: 'E',
+            scope: {
+                user: '=',
+                collapsed: "@"
+            },
+            controller: function ($scope) {
+                //console.log($scope.collapsed);
+                var uc = this;
+                console.log();
+                uc.knight = function (user) {
+                    user.rank = 'Knight';
+                    console.log(user);
+                };
+
+                uc.collapsed = ($scope.collapsed === 'true');
+
+                uc.collapse = function () {
+                    uc.collapsed = !uc.collapsed;
+                };
+            },
+            controllerAs: 'uc'
+        }
+    });
+
+angular
+.module('myApp')
+.directive('removeFriend', function () {
+        return {
+            templateUrl: 'remove-friend.html',
+            controller: function ($scope) {
+                var rf = this;
+                rf.removing = false;
+                rf.startRemove = function () {
+                    rf.removing = true;
+                };
+                rf.cancelRemove = function () {
+                    rf.removing = false;
+                };
+                rf.removeFriend = function (friend) {
+                    var idx = $scope.user.friends.indexOf(friend);
+                    if(idx > -1){
+                        $scope.user.friends.splice(idx,1);
+                    }
+                };
+            },
+            controllerAs: 'rf'
+        }
+    });
+
+angular
+    .module('myApp')
+    .directive('address', function () {
+        return {
+            templateUrl: 'address.html',
+            controller: function () {
+                var ad = this;
+                ad.collapsed = false;
+
+                ad.expandAddress = function () {
+                    ad.collapsed = false;
+                };
+
+                ad.collapseAddress = function () {
+                    ad.collapsed = true;
+                }
+            },
+            controllerAs: 'ad'
         }
     });
